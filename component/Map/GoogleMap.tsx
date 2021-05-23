9d381b941abf2c9ef745ace53f6e4d1369c33f02
panzerstadt <tliqun@gmail.com>
2021-05-24 01:00:32 +0900

    notes

--- component/Map/GoogleMap.tsx
+++ component/Map/GoogleMap.tsx
@@ -9,6 +9,8 @@ const containerStyle = {
 const DEFAULT_CENTER = { lat: 35.683470204356645, lng: 139.750344734758 };
 export type LatLng = typeof DEFAULT_CENTER;
 
+// if google maps popover is shitty, try this one: https://www.npmjs.com/package/react-tiny-popover
+
 export const GMap = ({ latlng = DEFAULT_CENTER }) => {
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GMAPS_API_KEY}>
